4714f2f7ff81682b6746000bd230d1220d3f260d
Gabriel-Avelino <avelinogabrieldossantos@gmail.com>
2023-07-31 11:10:57 -0300

    Finalização do Projeto

--- son_blog/blog/views.py
+++ son_blog/blog/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 
 # Create your views here.
+from blog.models import Post
+
+
 def home(request):
-    return render(request, 'home.html')
+    posts = Post.objects.all()
+    return render(request, 'home.html', {'posts': posts})
+
+
+def post(request, post_id):
+    posts = Post.objects.get(pk=post_id)
+    return render(request, 'post.html', {'post': posts})
